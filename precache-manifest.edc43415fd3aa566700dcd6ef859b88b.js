5a9e4771bd6bc5cc3db420dd58850693afe7b220
arsenmamataliev <a.mamataliev93@gmail.com>
2021-01-09 17:43:18 +0600

    Updates

---
+++ precache-manifest.edc43415fd3aa566700dcd6ef859b88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fa540949cf4cc954a24",
+    "url": "/weather-app/static/css/main.3eab9224.chunk.css"
+  },
+  {
+    "revision": "4fa540949cf4cc954a24",
+    "url": "/weather-app/static/js/main.cde0e387.chunk.js"
+  },
+  {
+    "revision": "d84817b9017832f572bf",
+    "url": "/weather-app/static/js/runtime~main.f5c8b83b.js"
+  },
+  {
+    "revision": "d8421ef0adabf2be2cda",
+    "url": "/weather-app/static/js/2.fa1e8574.chunk.js"
+  },
+  {
+    "revision": "a5a363b4de956bdbd41a",
+    "url": "/weather-app/static/js/3.8770fc33.chunk.js"
+  },
+  {
+    "revision": "258a0b2f2793657e826f004de7a8e600",
+    "url": "/weather-app/index.html"
+  }
+];
